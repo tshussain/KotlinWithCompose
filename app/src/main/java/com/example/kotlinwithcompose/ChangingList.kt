9b15b5a434ecef804202e5b6f385ee485292c95b
Talib Hussain <thussain@users.noreply.github.com>
2023-09-18 16:55:47 -0400

    Add fix for rememberSaveable (doesn't work with mutableStateList object).

--- app/src/main/java/com/example/kotlinwithcompose/ChangingList.kt
+++ app/src/main/java/com/example/kotlinwithcompose/ChangingList.kt
@@ -10,14 +10,40 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+/** We need to use this special remember function so that the list will
+ *    be "rememberSaveable" upon phone rotation, etc. */
+@Composable
+fun <T: Any> rememberMutableStateListOf(vararg elements: T): SnapshotStateList<T> {
+    return rememberSaveable(
+        saver = listSaver(
+            save = { stateList ->
+                if (stateList.isNotEmpty()) {
+                    val first = stateList.first()
+                    if (!canBeSaved(first)) {
+                        throw IllegalStateException("${first::class} cannot be saved. By default only types which can be stored in the Bundle class can be saved.")
+                    }
+                }
+                stateList.toList()
+            },
+            restore = { it.toMutableStateList() }
+        )
+    ) {
+        elements.toList().toMutableStateList()
+    }
+}
+
 @Composable
 fun ChangingList() {
-    val todoList = rememberSaveable { mutableStateListOf<String>() }
+    val todoList = rememberMutableStateListOf<String>() // use this to get rememberSaveable behavior
 
     LazyColumn {
         item() {
